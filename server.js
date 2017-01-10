2ac011229c2f5bc562efe33a2760f4a16a58029f
pingze <719695274@qq.com>
2017-01-10 22:40:01 +0800

    first work-fun version

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // server.js
 
 /**IMPORT**/
+var fs = require('fs');
 var express = require('express');
 var url = require('url');
 var bodyParser = require('body-parser');
@@ -20,6 +21,9 @@ function sendViewMiddleware(req, res, next) { //send .html
 }
 app.use(sendViewMiddleware);
 
+/**VARIABLE**/
+var data = {};
+
 /**GET**/
 app.get('/', function (req, res) { //主页
     console.log("[SERVER][GET] /");
@@ -27,19 +31,38 @@ app.get('/', function (req, res) { //主页
 });
 
 app.get('/img', function(req, res){
+    console.log("[SERVER][GET] /img");
     var params = url.parse(req.url,true).query;
-    console.log(params)
+    //console.log(params)
+    var id = params["id"];
     var q = params["q"];
     var t = params["t"];
-    getCaptcha.get(q,t,function(img){
+    getCaptcha.get(id,q,t,function(img,cookie){
+        data[id] = {
+            "q":q
+            ,"t":t
+            ,"cookie":cookie
+            ,"code":null //尚未获取
+        }
         res.send(img);
     })
 });
 
 app.post('/post', function (req, res) { //pipe
-    console.log("[SERVER][GET] /post");
-    console.log(req.body);
-    res.send("done");
+    console.log("[SERVER][POST] /post");
+    //console.log(req.body)
+    var id = req.body["id"];
+    var code = req.body["code"];
+    if(typeof(data[id])!="undefined"){
+        data[id]["code"] = code;
+        var json = JSON.stringify(data);
+        fs.writeFileSync('./captcha.json',json);
+        res.send("saved");
+        console.log("成功储存：");
+        console.log(data[id]);
+    }else{
+        res.send("invalid");
+    }
 });
 
 /**启动服务器**/
@@ -51,3 +74,7 @@ var server = app.listen(9999, function () {
 
 
 
+/*
+考虑按q分类数据
+考虑使用微型数据库存储和查询数据
+*/
